3e90219df7305fd46b8dc97815867b5e9162343e
Mahima <mahimaandani@gmail.com>
2022-04-21 23:09:58 +0530

    ant design

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd';
+
+function About() {
+  return (
+    <div>About
+        <Button type="primary">Button</Button>
+    </div>
+  )
+}
+
+export default About
