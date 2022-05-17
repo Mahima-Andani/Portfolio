f26ec223c443850e4ac84ac5cf459e81b046705a
Mahima <mahimaandani@gmail.com>
2022-05-17 12:30:55 +0530

    simple animation

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/skills/Skills";
 // import Threed from "./components/3dcomp/Threed";
-import { Canvas, AmbientLight } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import Fiber from "./components/3dcomp/Fiber";
 // import {OrbitControls} from 'drei'
 
@@ -18,13 +18,10 @@ function App() {
     <>
       <Canvas>
         <Suspense fallback= {null}>
-          <Fiber sides={[2,3,5]} color={'hotpink'}>
-            <Fiber sides={[1,2,4]} color={'grey'}>
-              <Fiber sides={[1,3,5]}>
-                <Fiber sides={[1,3,5]}/>
-              </Fiber>
-            </Fiber>
-          </Fiber>
+          <Fiber sides={[2,3,18]} color={'red'}/>
+          <Fiber sides={[1,2.5,18]} color={'orange'}/>
+          <Fiber sides={[1,2,18]} color={'yellow'}/>
+          <Fiber sides={[1,1.5,18]} color={'green'}/>
         </Suspense>
         {/* <OrbitControls/> */}
         <ambientLight intensity = {0.5}/>
--- src/components/3dcomp/Fiber.js
+++ src/components/3dcomp/Fiber.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useFrame } from '@react-three/fiber';
 // import {OrbitControls} from 'drei'
  
  function Fiber(props) {
+   const meshref = React.useRef()
+  useFrame(({clock}) => {
+    meshref.current.rotation.x = Math.sin(clock.getElapsedTime()) 
+    meshref.current.rotation.y = Math.sin(clock.getElapsedTime()) 
+  })
    return (
-     <mesh>
+     <mesh ref={meshref}>
        <ringGeometry attach='geometry' args={props.sides}/>
        <meshPhongMaterial attach='material' color={props.color}/>
      </mesh>
